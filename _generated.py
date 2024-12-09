0b14db7da7903f9ee7203019eeaf4594afdc5689
Matthew Angelakos <93948171+MattAngelakos@users.noreply.github.com>
2024-12-09 10:47:00 -0500

    s

--- _generated.py
+++ _generated.py
@@ -256,7 +256,7 @@ def query():
                             cursor_factory=psycopg2.extras.DictCursor)
     cur = conn.cursor()
     cur.execute("SELECT * FROM sales")
-    lines = 'SELECT ATTRIBUTE(S):\nprod, month, 1_sum_quant, 2_sum_quant, 3_sum_quant, 1_count_quant \nNUMBER OF GROUPING VARIABLES(n):\n1\nGROUPING ATTRIBUTES(V):\nprod\nF-VECT([F]):\n1_sum_quant, 2_sum_quant, 3_sum_quant, 1_count_quant\nSELECT CONDITION-VECT([σ]):\n1.month=1 and 1.year=2020 and 1.prod=prod\n2.month=2 and 2.year=2020 and 2.prod=prod\n3.month=3 and 3.year=2020 and 3.prod=prod\nHAVING_CONDITION(G):\n'
+    lines = 'SELECT ATTRIBUTE(S):\nprod, month, 1_count_quant, 2_count_quant\nNUMBER OF GROUPING VARIABLES(n):\n2\nGROUPING ATTRIBUTES(V):\nprod, month\nF-VECT([F]):\n1_count_quant, 2_count_quant, avg_quant\nSELECT CONDITION-VECT([σ]):\n1.prod=prod and 1.month=month-1 and 1.quant>avg_quant\n2.prod=prod and 2.month=month+1 and 2.quant>avg_quant\nHAVING_CONDITION(G):\n'
     emf_struct = process_txt(lines)
     rows = cur.fetchall()
     column_names = [description[0] for description in cur.description]
